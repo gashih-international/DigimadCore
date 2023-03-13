bec9d52553c3f2acdf01f46fe7a547438d5cab53
Роман Анпилов <>
2023-03-13 20:42:35 +0800

    Combine plus: add Feedback class

---
+++ Sources/DigimadCore/CombineFeatures/Combine+.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Роман Анпилов on 13.03.2023.
+//
+
+import Foundation
+import Combine
+
+@available(macOS 10.15, *)
+struct Feedback<State, Event> {
+    let run: (AnyPublisher<State, Never>) -> AnyPublisher<Event, Never>
+}
+
+@available(macOS 10.15, *)
+extension Feedback {
+    init<Effect: Publisher>(
+        effects: @escaping (State) -> Effect
+    ) where Effect.Output == Event, Effect.Failure == Never {
+        self.run = { state -> AnyPublisher<Event, Never> in
+            state
+                .map { effects($0) }
+                .switchToLatest()
+                .eraseToAnyPublisher()
+        }
+    }
+}
